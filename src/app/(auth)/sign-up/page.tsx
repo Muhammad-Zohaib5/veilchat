b1d1bb0536246f321d509eadcbd3880ce1b25db1
muhammad-zohaib5 <muhammadzohaib4852@gmail.com>
2025-08-06 11:28:52 +0500

    Add some fronted files

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -10,6 +10,12 @@ import { useRouter } from "next/navigation"
 import { signUpSchema } from "@/schemas/signUpSchema"
 import axios,{AxiosError} from 'axios'
 import { ApiResponse } from "@/types/ApiResponse"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Loader, Loader2 } from "lucide-react"
+
+
 
 
 const page = () => {
@@ -52,10 +58,111 @@ const page = () => {
       checkUsernameUnique()
     },[debouncedUsername])
 
-     
+    const onSubmit = async (data:z.infer<typeof signUpSchema>) => {
+      setIsSubmitting(true)
+      try {
+        const response = await axios.post<ApiResponse>('/api/sign-up',data)
+        toast.success(response.data.message)
+        router.replace(`/verify/${username}`)
+        setIsSubmitting(false)
+      } catch (error) {
+        console.log("Error in signup of user",error)
+         const axiosError = error as AxiosError<ApiResponse>;
+            let errorMessage = axiosError.response?.data.message
+            toast.error(errorMessage || "Signup failed")
+            setIsSubmitting(false)
+      }
+    } 
+
   return (
-    <div>
-      page
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div  className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md  ">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join VeilChat App
+          </h1>
+          <p className="mb-4">
+            Sign up to start your anonymous adventure
+          </p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+
+             <FormField
+            name="username"
+            control={form.control}
+            render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="username" 
+                {...field} 
+                onChange={(e)=>{
+                  field.onChange(e)
+                  setUsername(e.target.value)
+                }}
+                /> 
+              </FormControl>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+         <FormField
+            name="email"
+            control={form.control}
+            render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="email" 
+                {...field} 
+                /> 
+              </FormControl>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+         <FormField
+            name="password"
+            control={form.control}
+            render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="password" 
+                {...field} 
+                /> 
+              </FormControl>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" disabled={isSubmitting}>
+          {
+            isSubmitting ? (
+              <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin"/> Please wat
+              </>
+            ) : ('Signup')
+          }
+        </Button>
+
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Already a member?{''}
+            <Link href="/sign-in" className="text-blue-600 hover:text-blue-800">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }   
